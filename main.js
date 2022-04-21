0f24e59c7f9d93b8fd6ff3e1e40216124a048e4e
ccantoran <102254956+ccantoran@users.noreply.github.com>
2022-04-21 00:28:00 -0500

    FINAL VERSION

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-var span = document.getElementsByClassName('arrow');
-var div = document.getElementsByTagName('div');
-var l = 0;
-span[1].onclick = () =>{
-    l++;
-    for(var i of div)
-    {
-        if (l==0) {i.style.left = "0px";}
-        if (l==1) {i.style.left = "-740px";}
-        if (l==2) {i.style.left = "-1380px";}
-        if (l>3) {l=3;}
-    }
-}
-span[0].onclick = () =>{
-    l--;
-    for(var i of div)
-    {
-        if (l==0) {i.style.left = "0px";}
-        if (l==1) {i.style.left = "-740px";}
-        if (l==2) {i.style.left = "-1380px";}
-        if (l<0) {l=0;}
-    }
-}
+const mainMenu = document.querySelector('.mainMenu')
+const openMenu = document.querySelector('.openMenu')
+const closeMenu = document.querySelector('.closeMenu')
 
+
+document.querySelector('.openMenu').addEventListener('click', show);
+document.querySelector('.closeMenu').addEventListener('click', close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    openMenu.style.display = 'none';
+    document.querySelector('.clickli').addEventListener('click', close);
+}
+function close(){
+    mainMenu.style.display = "none";
+    openMenu.style.display = "flex";
+}
